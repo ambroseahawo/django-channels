f5e071a1db9b78cceb34ba8de92acaeafa85d8d1
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-01-03 23:57:51 +0300

    Urls

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from notification import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.notification_page_view, name="notification_page")
 ]
