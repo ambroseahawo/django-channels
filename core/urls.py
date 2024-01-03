3c494837b31b6556e24fadd77574fdd53db94618
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-01-04 00:28:39 +0300

    Implement consumers

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from notification import views
+from notification.consumers import NotificationConsumer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
