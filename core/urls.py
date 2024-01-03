e389745982fda135427495dfde432ed17df533cb
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-01-04 00:27:11 +0300

    Setup websockets

--- core/urls.py
+++ core/urls.py
@@ -22,3 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.notification_page_view, name="notification_page")
 ]
+
+websocket_urlpatterns = [
+    path("ws/notifications/", NotificationConsumer.as_asgi())
+]
